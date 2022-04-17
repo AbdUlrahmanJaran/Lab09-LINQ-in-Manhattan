ce3f7827a61058a20be8b4d64953d68a3ccc6a9e
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-04-17 08:51:46 +0300

    Lab 09

---
+++ Lab09-LINQ/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+
+namespace Lab09_LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../data.json";
+            string read = File.ReadAllText(path);
+
+            var jsonFile = JsonConvert.DeserializeObject<AllFeatures>(read);
+
+            List<Feature> features = jsonFile.Features;
+
+            var neighborhoods = from neighbor
+                                in features
+                                select neighbor.Properties.Neighborhood;
+
+            var neighborhoodFilter = from neighbor
+                                           in neighborhoods
+                                           where (!neighbor.Equals(""))
+                                           select neighbor;
+
+            var Distinct = neighborhoods.Select(neighbor => neighbor).Distinct().Where(neighbor => !(neighbor.Equals("")));
+
+            string userInput;
+            int userInputInt = 1;
+
+            Console.WriteLine($"Neighborhoods (All): {neighborhoods.Count()}");
+            Console.WriteLine($"Neighborhoods (Filtered): {neighborhoodFilter.Count()}");
+            Console.WriteLine($"Neighborhoods (Distinct): {Distinct.Count()}");
+            Console.WriteLine();
+
+            while (userInputInt != 0)
+            {
+                Console.WriteLine("Select");
+                Console.WriteLine("1. Display all neighborhoods");
+                Console.WriteLine("2. Display filtered neighborhoods");
+                Console.WriteLine("3. Display distinct neighborhoods");
+                Console.WriteLine("0. Exit");
+                userInput = Console.ReadLine();
+
+                try
+                {
+                    userInputInt = Convert.ToInt32(userInput);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                switch (userInputInt)
+                {
+                    case 1:
+                        foreach (var neighborhood in neighborhoods)
+                        {
+                            Console.WriteLine(neighborhood);
+                        }
+                        break;
+
+                    case 2:
+                        foreach (var neighborhood in neighborhoodFilter)
+                        {
+                            Console.WriteLine(neighborhood);
+                        }
+                        break;
+
+                    case 3:
+                        foreach (var neighborhood in Distinct)
+                        {
+                            Console.WriteLine(neighborhood);
+                        }
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class AllFeatures
+    {
+        [JsonProperty("features")]
+        public List<Feature> Features { get; set; }
+    }
+
+    public class Properties
+    {
+        [JsonProperty("neighborhood")]
+        public string Neighborhood { get; set; }
+    }
+
+    public class Feature
+    {
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab09-LINQ-in-Manhattan
+This is a Console apllication aim to Read and Filter data from a json file.
+
+## Visual
+Here is the start of the aplication:<br>
+![Select](./images/Select.png)
+
+we can select "1" for All Neighborhoods or "2" for only neighborhoods with Names or "3" for a list of Neighborhoods without duplicate or "0" to Exit.
+
+The third option: <br>
+![Distinct](/images/Distinct.png)
